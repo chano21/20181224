0b436e60218c4aedadd1b20da7999ed6e8fa0967
chano <chano22@naver.com>
2018-12-28 01:26:52 +0900

    dd

---
+++ PCOProject/src/test/java/com/crazypco/project/JpaTest.java
@@ -0,0 +1,18 @@
+package com.crazypco.project;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@ContextConfiguration(locations = "classpath:application-context-test.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class JpaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
+https://howtodoinjava.com/best-practices/how-you-should-unit-test-dao-layer/

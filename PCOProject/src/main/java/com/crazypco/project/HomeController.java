c24c95b81cb26b9273bd7f8b3ab6179f8d9b4a25
chano <chano22@naver.com>
2019-04-05 01:21:44 +0900

    dd

--- PCOProject/src/main/java/com/crazypco/project/HomeController.java
+++ PCOProject/src/main/java/com/crazypco/project/HomeController.java
@@ -25,7 +25,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale issssss {}.", locale);
-		System.err.println();
+		System.err.println("testbranch");
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
